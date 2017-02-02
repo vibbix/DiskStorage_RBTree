5b7859f18dc7bdb497295163a963880b198015d0
Mark Beznos <mark.vibbix@gmail.com>
2017-02-02 13:17:01 -0500

    Initial work

--- src/edu/wit/cs/comp2350/RBTree.java
+++ src/edu/wit/cs/comp2350/RBTree.java
@@ -1,14 +1,17 @@
 package edu.wit.cs.comp2350;
 
+import java.util.Collection;
+
 public class RBTree extends LocationHolder {
-	
+
 	private DiskLocation nil = new DiskLocation(-1, -1);
+	private DiskLocation root = null;
 
 	private void setRed(DiskLocation z) {
 		if (z != nil)
 			z.color = RB.RED;
 	}
-	
+
 	@Override
 	public DiskLocation find(DiskLocation d) {
 		// TODO Auto-generated method stub
@@ -17,20 +20,28 @@ public class RBTree extends LocationHolder {
 
 	@Override
 	public DiskLocation next(DiskLocation d) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.succ(d);
 	}
 
 	@Override
 	public DiskLocation prev(DiskLocation d) {
-		// TODO Auto-generated method stub
-		return null;
+		return d.parent;
 	}
 
 	@Override
 	public void insert(DiskLocation d) {
-		// TODO Auto-generated method stub
-
+		d.parent = this.findParent(d, this.root, nil);
+		if (d.parent == null || d.parent.equals(nil)) {
+			this.root = d;
+		} else {
+			if (d.parent.isGreaterThan(d)) {
+				d.parent.left = d;
+			} else {
+				d.parent.right = d;
+			}
+		}
+		d.left = nil;
+		d.right = nil;
 	}
 
 	@Override
@@ -39,4 +50,42 @@ public class RBTree extends LocationHolder {
 		return 0;
 	}
 
+	private DiskLocation findParent(DiskLocation n, DiskLocation curr, DiskLocation parr) {
+		if (curr == null) {
+			return parr;
+		}
+		if (curr.isGreaterThan(n)) {
+			return findParent(n, curr.left, curr);
+		} else {
+			return findParent(n, curr.right, curr);
+		}
+	}
+
+	private DiskLocation succ(DiskLocation d) {
+		if (d.right != null)
+			return this.minValue(d);
+		return up(d);
+	}
+
+	private DiskLocation up(DiskLocation d) {
+		DiskLocation p = d.parent;
+		if ((p.parent == null) || (d.parent.equals(this.nil))|| (d == p.left)) {
+			return p;
+		}
+		return up(p);
+	}
+
+	private DiskLocation minValue(DiskLocation d) {
+		DiskLocation dl = d;
+		while (dl.right != null) {
+			dl = dl.right;
+		}
+		return dl;
+	}
+
+	//private void fixInsert(DiskLocation d){
+	//	while(d.parent.color == RB.RED){
+	//		if (d.parent)
+	//	}
+	//}
 }
