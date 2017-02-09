dc56a05b2192bd1aae3f855d7660baa930fdf8e1
Mark Beznos <mark.vibbix@gmail.com>
2017-02-09 00:37:05 -0500

    various changes

--- src/edu/wit/cs/comp2350/RBTree.java
+++ src/edu/wit/cs/comp2350/RBTree.java
@@ -1,126 +1,197 @@
 package edu.wit.cs.comp2350;
 
+import java.awt.*;
 import java.util.Collection;
 
 public class RBTree extends LocationHolder {
-
-	private static final DiskLocation nil = new DiskLocation(-1, -1);
-	private DiskLocation root = null;
-
-	private void setRed(DiskLocation z) {
-		if (z != nil)
-			z.color = RB.RED;
+	private final static DiskLocation nil = new DiskLocation(-1, -1);
+    private DiskLocation root = null;
+	private void setRed(DiskLocation d) {
+        if (isNull(d)){
+            return;
+        }
+		if (d != nil)
+			d.color = RB.RED;
 	}
-
+	/**
+	 * If dislocation <em>d</em> is already in the tree, this method returns
+	 * the equivalent location with the relevant metadeta. Otherwise returns
+	 * null.
+	 * @param d disk location to search for
+	 * @return disk location with metadeta; if not in database, it's in the
+	 */
 	@Override
 	public DiskLocation find(DiskLocation d) {
-		// TODO Auto-generated method stub
+        return find(d, this.root);
+	}
+
+
+
+    /**
+     * Gets the next disklocation.
+     * @param d DiskLocation
+     * @return the next disklocation
+     */
+    @Override
+	public DiskLocation next(DiskLocation d) {
+        if (isNull(d)){
+            return nil;
+        }
+        // TODO Auto-generated method stub
+        // TODO make sure doesn't fail if next items are all equal
 		return null;
 	}
 
-	@Override
-	public DiskLocation next(DiskLocation d) {
-		return this.succ(d);
-	}
-
+	/**
+	 * @param d disk location use for search
+	 * @return The previous location.
+	 */
 	@Override
 	public DiskLocation prev(DiskLocation d) {
+        if (isNull(d)){
+            return nil;
+        }
 		return d.parent;
 	}
 
+	/**
+     * Inserts a new disk order
+	 * @param d DiskLocation to insert
+	 */
 	@Override
 	public void insert(DiskLocation d) {
-//	    insert(this.root, d);
-		d.parent = this.findParent(d, this.root, nil);
-		if (d.parent == null || d.parent.equals(nil)) {
-			this.root = d;
-		} else {
-			if (d.parent.isGreaterThan(d)) {
-				d.parent.left = d;
-			} else {
-				d.parent.right = d;
-			}
-		}
-		d.left = nil;
-		d.right = nil;
-
-	}
-
-	@Override
-	public int height() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private DiskLocation findParent(DiskLocation n, DiskLocation curr, DiskLocation parr) {
-		if (curr == null) {
-			return parr;
-		}
-		if (curr.isGreaterThan(n)) {
-			return findParent(n, curr.left, curr);
-		} else {
-			return findParent(n, curr.right, curr);
-		}
-	}
-
-	private DiskLocation succ(DiskLocation d) {
-		if (d.right != null && !d.right.equals(nil))
-			return this.minValue(d);
-		return up(d);
-	}
-
-	private DiskLocation up(DiskLocation d) {
-		DiskLocation p = d.parent;
-		if ((p.parent == null) || (d.parent.equals(this.nil)) || (d == p.left)) {
-			return p;
-		}
-		return up(p);
-	}
-
-	private DiskLocation minValue(DiskLocation d) {
-		DiskLocation dl = d;
-		while (dl.right != null) {
-			dl = dl.right;
-		}
-		return dl;
-	}
-
-	private void fixInsert(DiskLocation d){
-		while(d.parent.color == RB.RED){
-            if (d.parent.equals(d.parent.parent.left)){
-                DiskLocation y = d.parent.parent.right;
-                if (y.color == RB.RED){
-                    d.parent.color = RB.BLACK;
-                    y.color = RB.BLACK;
-                    d.parent.parent.color = RB.RED;
-                    d = d.parent.parent;
-                } else {
-                    if(d.equals(d.parent.right)){
-                        d = d.parent;
-                        rotateLeft(d);
-                    }
-                    d.parent.color = RB.BLACK;
-                    d.parent.parent.color = RB.RED;
-                    rotateRight(d.parent.parent);
+	    if (isNull(d)){
+	        return;
+        }
+        if (isNull(this.root)) {
+            this.root = d;
+            return;
+        }
+        DiskLocation s = this.root;
+        while(true) {
+            int compare = (d.isGreaterThan(s))? 1 : -1;
+            if (compare == -1)
+                if(d.equals(s))
+                    compare = 0;
+            if (compare == 0) {
+                this.root = s;
+            } else if (compare < 0){
+                if (isNull(getLeftOf(s)))
+                {
+                    s.left = d;
+                    d.right = s;
+                    fixAfterInsert(s.left);
+                    break;
                 }
             } else {
-                this.swapChild(d);
+                if (isNull(getRightOf(s))){
+                    s.right = d;
+                    d.left = s;
+                    fixAfterInsert(s.right);
+                    break;
+                }
             }
-		}
-		this.root.color = RB.BLACK;
+            s = getRightOf(s);
+        }
 	}
-	private void rotateRight(DiskLocation d){
 
+    /**
+     * Gets the black-node leaning height of the tree
+     * @return height of the tree
+     */
+    @Override
+	public int height() {
+        // TODO Auto-generated method stub
+		return 0;
+	}
+	// beginregion helpers
+    /**
+     * @param d disklocation to search
+     * @return grandparent of d; otherwise null
+     */
+    private DiskLocation getGrandParentOf(DiskLocation d) {
+        return  isNull(d) || isNull(d.parent) ? nil : d.parent.parent;
+    }
+    private DiskLocation getSiblingOf(DiskLocation d) {
+        return isNull(d) || isNull(d.parent) ? nil :
+                (d.equals(d.parent.left)) ? d.parent.right : d.parent.left;
+    }
+    private boolean isNull(DiskLocation d) {
+        return d == null || d.equals(nil);
+    }
+    private DiskLocation getLeftOf(DiskLocation d){
+        return isNull(d) ? nil : d.left;
+    }
+    private DiskLocation getRightOf(DiskLocation d){
+        return isNull(d) ? nil : d.right;
+    }
+    private DiskLocation find(DiskLocation find, DiskLocation current){
+        if (isNull(find) || isNull(current)){
+            if (find.equals(current))
+                return current;
+            if (current.isGreaterThan(current))
+                return find(find, current.left);
+            return find(find, current.right);
+        }
+        return nil;
+    }
+    private void fixAfterInsert(DiskLocation d){
+        if (isNull(d)){
+            return;
+        }
+        d.color = RB.RED;
+        if (!d.equals(root))
+            if (d.parent.color == RB.RED){
+                if(getSiblingOf(d.parent).color == RB.RED){
+                    d.parent.color = RB.BLACK;
+                    getSiblingOf(d.parent).color = RB.BLACK;
+                    getGrandParentOf(d).color = RB.BLACK;
+                    fixAfterInsert(getGrandParentOf(d));
+                } else if (d.parent.equals(getGrandParentOf(d).left)){
+                    if (getRightOf(d.parent).equals(d)){
+                        rotateLeft(d = d.parent);
+                    }
+                    d.parent.color = RB.BLACK;
+                    getGrandParentOf(d).color = RB.RED;
+                    rotateRight(getGrandParentOf(d));
+                } else if (d.parent.equals(getGrandParentOf(d).right)){
+                    if (getLeftOf(d.parent).equals(d)) {
+                        rotateRight(d = d.parent);
+                    }
+                    d.parent.color = RB.BLACK;
+                    getGrandParentOf(d).color = RB.RED;
+                    rotateLeft(getGrandParentOf(d));
+                }
+            }
+        this.root.color = RB.BLACK;
     }
     private void rotateLeft(DiskLocation d){
-
+        if  (isNull(getRightOf(d)))
+            return;
+        DiskLocation oldright = d.right;
+        d.right = oldright.right;
+        if (isNull(d.parent)) {
+            this.root = oldright;
+        } else if (d.parent.left.equals(d)){
+            d.parent.left = oldright;
+        } else {
+            d.parent.right = oldright;
+        }
+        oldright.left = d;
     }
-    private void swapChild(DiskLocation d){
-        DiskLocation temp = d.left;
-        d.left = d.right;
-        d.right = temp;
+    private void rotateRight(DiskLocation d){
+        if  (isNull(getLeftOf(d)))
+            return;
+        DiskLocation oldleft = d.left;
+        d.left = oldleft.left;
+        if (isNull(d.parent)) {
+            this.root = oldleft;
+        } else if (d.parent.left.equals(d)){
+            d.parent.left = oldleft;
+        } else {
+            d.parent.right = oldleft;
+        }
+        oldleft.right = d;
     }
-    private static boolean isNil(DiskLocation d){
-        return (d == null) || (d.equals(nil));
-    }
+    // endregion
 }
